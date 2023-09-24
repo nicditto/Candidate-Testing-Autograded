4344853459467b16c9d2a17c421d51a7f1b7a4a7
Nicole Ditto <nicoleditto@Nicoles-Air.lan>
2023-09-24 10:31:53 -0500

    Update9/24

--- candidate-testing.js
+++ candidate-testing.js
@@ -33,7 +33,7 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly update// 
 console.log(`You answered ${candidateAnswers[i]} the correct answer is ${correctAnswers[i]}! `)
 
 
